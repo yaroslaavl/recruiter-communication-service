e1b385608919d36d28521f458cf33cd7f3c35778
ylopatkin <ylopatkin@gmail.com>
2025-09-27 23:02:17 +0200

    27.09 - Add Feign client, token management, and DTO for `communication-service`.

---
+++ src/main/java/org/yaroslaavl/communicationservice/feignClient/RecruitingFeignClient.java
@@ -0,0 +1,20 @@
+package org.yaroslaavl.communicationservice.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.yaroslaavl.communicationservice.config.FeignConfig;
+import org.yaroslaavl.communicationservice.feignClient.dto.ApplicationChatInfo;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@FeignClient(name = "recruiting-service", path = "/api/v1", configuration = FeignConfig.class)
+public interface RecruitingFeignClient {
+
+    @GetMapping("/applications/chat-open")
+    boolean isOpenedChatting(@RequestParam("applicationId") UUID applicationId);
+
+    List<ApplicationChatInfo> getPreviewApplications(@RequestParam("applicationIds") Set<UUID> applicationIds);
+}
